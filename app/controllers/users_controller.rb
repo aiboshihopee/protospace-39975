1bcbfef3dddb2ff84b2176f8f5710f594fd8750c
aiboshihopee <aiboshihopee@gmail.com>
2024-02-21 00:48:09 +0900

    ユーザー詳細機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @prototype_params = @user.prototypes.includes(:user)
+    @prototype = @user.prototypes.includes(:user)
   end
 end
